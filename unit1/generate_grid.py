dbca3b5ebeeab174e1e60648c85bbcc24ebab42b
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2018-06-13 17:19:10 +0300

    generate_grid.py добавлена функция get_int()

--- unit1/generate_grid.py
+++ unit1/generate_grid.py
@@ -2,4 +2,17 @@ import random
 
 
 def get_int(msg, minimum, default):
-    pass
+    while True:
+        try:
+            line = input(msg)
+            if not line and default is not None:
+                return default
+            i = int(line)
+            if i < minimum:
+                print('Должен быть >=', minimum)
+            else:
+                return i
+        except ValueError as err:
+            print(err)
+
+
