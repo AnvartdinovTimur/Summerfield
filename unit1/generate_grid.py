374371ed7bfbe1ebca575d49af8cb66859ab99b7
anvartdinovtimurlinux <anvartdinovtimurlinux@gmail.com>
2018-06-13 18:01:17 +0300

    generate_grid.py v1.0

--- unit1/generate_grid.py
+++ unit1/generate_grid.py
@@ -16,3 +16,27 @@ def get_int(msg, minimum, default):
             print(err)
 
 
+rows = get_int('Строк: ', 1, None)
+columns = get_int('Столбцов: ', 1, None)
+minimum = get_int('Минимальное значение (нажмите Enter для 0): ',
+                  -1000000, 0)
+
+default = 1000
+if default < minimum:
+    default = 2 * minimum
+maximum = get_int('Максимальное значение (нажмите Enter для {0}: '
+                  .format(str(default)), minimum, default)
+
+row = 0
+while row < rows:
+    line = ''
+    column = 0
+    while column < columns:
+        i = random.randint(minimum, maximum)
+        s = str(i)
+        while len(s) < 10:
+            s = ' ' + s
+        line += s
+        column += 1
+    print(line)
+    row += 1
